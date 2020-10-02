ba31f70a3636e7806962fae6b8a4f7ed9067adee
harshit0501 <harshitgoel0501@gmail.com>
2020-10-02 21:51:21 +0530

    Refined lists page

--- scripts/list.js
+++ scripts/list.js
@@ -1,4 +1,4 @@
-
+let colors = ["#FFFF33", "#FF0000", "#00FF00", "#00FFFF", "#FF00FF", "#FF6600"];
 const data = [
   {
     name: "Platinum",
@@ -7,27 +7,27 @@ const data = [
   },
   {
       name: "Gold",
-      description: "description",
+      description: "This Package includes the sports with only Earth Like gravity",
       price: "3500/month",
   },
   {
       name: "Bronze",
-      description: "description",
+      description: "This Package includes the sports with only normal gravity",
       price: "2500/month",
   },
   {
       name: "Gym-only",
-      description: "description",
+      description: "This package includes Gym and spa",
       price: "1000/month",
   },
   {
       name: "swim and gym",
-      description: "description",
+      description: "This package includes Gym, spa and swimming pool",
       price: "1500/month"
   },
   {
       name: "kids package",
-      description: "description",
+      description: "This package includes football, basketball, and badminton",
       price: "1000/month",
   },
   
@@ -35,14 +35,16 @@ const data = [
 const header = document.querySelectorAll(".header");
 const price = document.querySelectorAll(".price");
 const description = document.querySelectorAll(".description");
-
+const button = document.querySelectorAll('.btn');
+const cards = document.querySelectorAll('.card');
 let i;
 for(i = 0; i < 6; i++) {
     
     header[i].textContent = data[i].name;
     description[i].textContent = data[i].description;
     price[i].textContent = data[i].price;
+    price[i].style.color = colors[i];
+    button[i].style.backgroundColor = colors[i];
+    cards[i].style.borderColor = colors[i];
 }
-
-
 
