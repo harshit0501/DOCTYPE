6c85b00e4a3021171eb892628172bcac59c679c3
Tushar Maroo <maroo.tushar@gmail.com>
2020-10-03 22:47:37 +0530

    Add new Bill Page

---
+++ scripts/bill.js
@@ -0,0 +1,56 @@
+let packageCost = localStorage.getItem("amount");
+let time = localStorage.getItem("subscription");
+let package = localStorage.getItem("package");
+let Name    = localStorage.getItem("Name");
+let Name1   = localStorage.getItem("Name1");
+let Name2   = localStorage.getItem("Name2");
+let Name3   = localStorage.getItem("Name3");
+let contact = localStorage.getItem("contact");
+let email   = localStorage.getItem("email");
+let houseN  = localStorage.getItem("houseN");
+let settleN = localStorage.getItem("settleN");
+let planet  = localStorage.getItem("planet");
+let serTax  = localStorage.getItem("serTax");
+let GST     = localStorage.getItem("GST");
+let TotCost = localStorage.getItem("TotCost");
+
+if(package==1)
+{
+    document.querySelector("#billName1").classList.remove("hide");
+}
+if(package==2)
+{
+    document.querySelector("#billName1").classList.remove("hide");
+    document.querySelector("#billName2").classList.remove("hide");
+    document.querySelector("#billName3").classList.remove("hide");
+}
+let d = new Date();
+let date = d.getDate();
+let month = d.getMonth();
+let year = d.getFullYear();
+let endDate;
+if(time == 0) {
+    endDate = `${date}/${month+1}/${year+1}`;
+} else if(time == 1) {
+    if(month+2 > 12) {
+        endDate = `${date}/${month+2-12}/${year+1}`;
+    }
+    endDate = `${date}/${month + 2}/${year}`;
+}
+document.querySelector("#billStartDate").textContent = `${date}/${month+1}/${year}`;
+document.querySelector("#billEndDate").textContent = endDate;
+document.querySelector("#billName").textContent = Name;
+document.querySelector("#billNamee").textContent = Name;
+document.querySelector("#billName1").textContent = Name1;
+document.querySelector("#billName2").textContent = Name2;
+document.querySelector("#billName3").textContent = Name3;
+document.querySelector("#billEmail").textContent = email;
+document.querySelector("#billHouseN").textContent = houseN;
+document.querySelector("#billSettleN").textContent = settleN;
+document.querySelector("#billPlanet").textContent = planet;
+document.querySelector("#billHouseN").textContent = houseN;
+document.querySelector("#billContact").textContent = contact;
+document.querySelector("#billCost").textContent = packageCost;
+document.querySelector("#billSerTax").textContent = serTax;
+document.querySelector("#billGST").textContent = GST;
+document.querySelector("#billTotal").textContent = TotCost;
--- scripts/form.js
+++ scripts/form.js
@@ -7,50 +7,44 @@ const name3 = document.querySelector("#name3")
 if(package==1)
 {
     name1.classList.remove("hide");
-    document.querySelector("#billName1").classList.remove("hide");
 }
 if(package==2)
 {
     name1.classList.remove("hide");
     name2.classList.remove("hide");
     name3.classList.remove("hide");
-    document.querySelector("#billName1").classList.remove("hide");
-    document.querySelector("#billName2").classList.remove("hide");
-    document.querySelector("#billName3").classList.remove("hide");
 }
 const form = document.querySelector("form");
 const body = document.querySelector("body");
 const bill = document.querySelector(".bill");
 
 form.addEventListener("submit",() => {
-    body.style.backgroundColor = "white";
-    form.style.display = "none";
-    const Name = document.querySelector("#name").value;
-    const Name1 = document.querySelector("#name1").value;
-    const Name2 = document.querySelector("#name2").value;
-    const Name3 = document.querySelector("#name3").value;
+    const Name    = document.querySelector("#name").value;
+    const Name1   = document.querySelector("#name1").value;
+    const Name2   = document.querySelector("#name2").value;
+    const Name3   = document.querySelector("#name3").value;
     const contact = document.querySelector("#contact").value;
-    const email = document.querySelector("#email").value;
-    const houseN = document.querySelector("#houseN").value;
+    const email   = document.querySelector("#email").value;
+    const houseN  = document.querySelector("#houseN").value;
     const settleN = document.querySelector("#settleN").value;
-    const planet = document.querySelector("#planet").value;
-    const serTax = (packageCost*2)/100;
-    const GST = (packageCost*5)/100;
+    const planet  = document.querySelector("#planet").value;
+    const serTax  = (packageCost*2)/100;
+    const GST     = (packageCost*5)/100;
     const TotCost = (packageCost*107)/100;
-    document.querySelector("#billName").textContent = Name;
-    document.querySelector("#billNamee").textContent = Name;
-    document.querySelector("#billName1").textContent = Name1;
-    document.querySelector("#billName2").textContent = Name2;
-    document.querySelector("#billName3").textContent = Name3;
-    document.querySelector("#billEmail").textContent = email;
-    document.querySelector("#billHouseN").textContent = houseN;
-    document.querySelector("#billSettleN").textContent = settleN;
-    document.querySelector("#billPlanet").textContent = planet;
-    document.querySelector("#billHouseN").textContent = houseN;
-    document.querySelector("#billContact").textContent = contact;
-    document.querySelector("#billCost").textContent = packageCost;
-    document.querySelector("#billSerTax").textContent = serTax;
-    document.querySelector("#billGST").textContent = GST;
-    document.querySelector("#billTotal").textContent = TotCost;
-    bill.style.display = "block";
+    localStorage.setItem("amount", packageCost);
+    localStorage.setItem("package", time);
+    localStorage.setItem("package", package);
+    localStorage.setItem("Name", Name);
+    localStorage.setItem("Name1", Name1);
+    localStorage.setItem("Name2", Name2);
+    localStorage.setItem("Name3", Name3);
+    localStorage.setItem("contact", contact);
+    localStorage.setItem("email", email);
+    localStorage.setItem("houseN", houseN);
+    localStorage.setItem("settleN", settleN);
+    localStorage.setItem("planet", planet);
+    localStorage.setItem("serTax", serTax);
+    localStorage.setItem("GST", GST);
+    localStorage.setItem("TotCost", TotCost);
+    window.location.href = "./bill.html";
 });
