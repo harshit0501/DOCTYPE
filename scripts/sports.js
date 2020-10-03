a6d83e1a96b5c1b1848490427e093fa5c2015747
harshit0501 <harshitgoel0501@gmail.com>
2020-10-03 16:43:12 +0530

    SearchBar added

--- scripts/sports.js
+++ scripts/sports.js
@@ -145,35 +145,89 @@ const sports = [
     },
 ];
 
-const header = document.querySelectorAll('.header');
-const image = document.querySelectorAll('.image');
-const trainer = document.querySelectorAll('.trainer');
-const galaxoEarth = document.querySelectorAll('.earth');
-const usericon = document.querySelectorAll('.fa-user');
-const earthicon = document.querySelectorAll('.fa-globe-asia');
-let i;
-const green = 'lightgreen' ;
-const red= "red";
-for(i = 0; i<sports.length;i++) {
-    header[i].textContent = sports[i].sport;
-    image[i].src = sports[i].image;
-    if(sports[i].isTrainer) {
-        trainer[i].textContent = '  Trainer available';
-        usericon[i].style.color = green;
-    } else {
-        trainer[i].textContent = '  Trainer not available';
-        usericon[i].style.color = red;
-    }
 
-    if (sports[i].isGalaxoEarth) {
-      galaxoEarth[i].textContent = "  GalaxoEarth available";
-      earthicon[i].style.color = green;
-    } else {
-      galaxoEarth[i].textContent = "  GalaxoEarth not available";
-      earthicon[i].style.color = red;
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+
+  let sport = searchSport(searchString, sports);
+  build(sport);
+});
+
+function searchSport(value, data) {
+  let filteredSports = [];
+
+  for (let i = 0; i < data.length; i++) {
+    value = value.toLowerCase();
+    let name = data[i].sport.toLowerCase();
+
+    if (name.includes(value)) {
+      filteredSports.push(data[i]);
     }
+  }
+  return filteredSports;
+}
+
+build(sports);
+function build(data) {
+  let sportcard = document.getElementById("container");
+  sportcard.innerHTML = "";
+
+  let i;
+  for (i = 0; i < data.length; i++) {
+    let trainer = data[i].isTrainer
+      ? "Trainer is available"
+      : "Trainer is not available";
+    let earth = data[i].isGalaxoEarth
+      ? "GalaxoEarth is available"
+      : "GalaxoEarth is not available";
+    let card = `<div class="card">
+        <h3 class="header">${data[i].sport}</h3>
+        <div class="content">
+          <img class="image" src="${data[i].image}" alt="" />
+          <div class="label">
+            <div class="t">
+              <span><i class="fas fa-user"></i><span class="trainer"></span> ${trainer} </span>
+          </div>
+          <div class="e">
+              <span><i class="fas fa-globe-asia"></i><span class="earth"></span> ${earth}</span>
+          </div>
+          </div>
+        </div>`;
+    sportcard.innerHTML += card;
+  }
+}
+
+// const header = document.querySelectorAll('.header');
+// const image = document.querySelectorAll('.image');
+// const trainer = document.querySelectorAll('.trainer');
+// const galaxoEarth = document.querySelectorAll('.earth');
+// const usericon = document.querySelectorAll('.fa-user');
+// const earthicon = document.querySelectorAll('.fa-globe-asia');
+// let i;
+// const green = 'lightgreen' ;
+// const red= "red";
+// for(i = 0; i<sports.length;i++) {
+//     header[i].textContent = sports[i].sport;
+//     image[i].src = sports[i].image;
+//     if(sports[i].isTrainer) {
+//         trainer[i].textContent = '  Trainer available';
+//         usericon[i].style.color = green;
+//     } else {
+//         trainer[i].textContent = '  Trainer not available';
+//         usericon[i].style.color = red;
+//     }
+
+//     if (sports[i].isGalaxoEarth) {
+//       galaxoEarth[i].textContent = "  GalaxoEarth available";
+//       earthicon[i].style.color = green;
+//     } else {
+//       galaxoEarth[i].textContent = "  GalaxoEarth not available";
+//       earthicon[i].style.color = red;
+//     }
 
     
 
-    galaxoEarth[i].textContent = (sports[i].isGalaxoEarth)? '  GalaxoEarth is available': '  GalaxoEarth is not available';
-}
+//     galaxoEarth[i].textContent = (sports[i].isGalaxoEarth)? '  GalaxoEarth is available': '  GalaxoEarth is not available';
+// }
